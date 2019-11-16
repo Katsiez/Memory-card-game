aeae54e885c6c47544f2d296ebd9371ff5165673
Katsiez <katarinajovanovic.a@gmail.com>
2019-11-16 01:16:58 +0100

    script.js
    Added background music! This is huge considering I could not do this before! So excited!

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
-if(document.readyState ==='loading') {
-    document.addEventListener('DOMContentLoaded', ready());
-} else {
-    ready();
+class AudioController {
+    constructor() {
+        this.bgMusic = new Audio('assets/audio/bgmusic.mp3');
+        this.bgMusic.volume = 0.5;
+        this.bgMusic.loop = true;
+        this.flipSound = new Audio('assets/audio/cardflip.wav');
+        this.matchSound = new Audio('assets/audio/match.wav');
+        this.victorySound = new Audio('assets/audio/victory.wav');
+        this.gameOverSound = new Audio('assets/audio/gameover.wav');
+    }
+    startMusic() {
+        this.bgMusic.play();
+    }
 }
+
+
 function ready() {
     let overlays = Array.from(document.getElementsByClassName('overlay-text'));
     let cards = Array.from(document.getElementsByClassName('card'));
@@ -10,6 +21,21 @@ function ready() {
     overlays.forEach(overlay => {
         overlay.addEventListener('click', () => {
             overlay.classList.remove('visible');
+            // game.startGame();
+            let audioController = new AudioController();
+            audioController.startMusic();
+        });
+    });
+    cards.forEach(card => {
+        card.addEventListener('click', () => {
+            // game.flipCard(card);
         });
     });
 }
+if (document.readyState === 'loading') {
+    document.addEventListener('DOMContentLoaded', ready());
+} else {
+    ready();
+}
+
+
